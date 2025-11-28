5d386668e0ed235abf262aff250dd2ae30d39d60
RIDINROUND <martinezsanchezj171@gmail.com>
2025-11-28 13:43:20 -0600

    Malilla

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -2,7 +2,7 @@ const Cliente = require ('../models/cliente');
 //const clienteModel = require('../models/cliente');
 const clienteController = {};
 
-
+// MALILLA XD
 
 //REGISTRAR CLIENTE
 clienteController.registrarCliente = async (req, res, next) => {
