b1d1d639efe73b8247a5a55e0d632cfdbf3da47b
Manuel <manuhh14@gmail.com>
2026-01-13 10:01:13 -0600

    conexion reestablecida a la base de datos

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ const types = pgp.pg.types;
 types.setTypeParser(1114, (stringValue) => stringValue);
 
 //URL EXTERNA DE RENDER (con SSL)
-const DB_URL = 'postgresql://inventario_666p_user:FH8EPWjRBjl1W4bu1nNSxkiobxAvCpPS@dpg-d4j1q27diees738fj7h0-a.oregon-postgres.render.com/inventario_666p?ssl=true';
+const DB_URL = 'postgresql://inventario69_user:RXnkZ9JMXbojW7WKP093ZxJLq1uH0CPo@dpg-d5j6c1chg0os73ehjda0-a.oregon-postgres.render.com/inventario69?ssl=true';
 
 const db = pgp(DB_URL);
 
